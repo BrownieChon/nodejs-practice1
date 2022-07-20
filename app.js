9ff8d084cc0812429467ea4f2f8173580e14a23b
BrownieChon <106335808+BrownieChon@users.noreply.github.com>
2022-07-20 22:29:27 +0700

    update index

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 // call modules
+const path = require('path')
+// call path
+
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")))
+// set static path
 
 app.get("/", (req,res) => {
-// get request from app.domain/ 
+// set url path
 
     res.send('<h1>Say Hi</h1> <br> <h2>Hello</h2>');
     // send respond
