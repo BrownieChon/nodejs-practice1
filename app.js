090d69b3802eac0e408f9531e12f0e63d2d936ad
BrownieChon <106335808+BrownieChon@users.noreply.github.com>
2022-07-21 18:05:11 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // main app
 
+// Import module
 const express = require('express');
 const chalk = require('chalk');
 // change color module
@@ -10,20 +11,51 @@ const port = process.env.port;
 // call modules
 const path = require('path')
 // call path
-
+const products = require('./data/products.json')
+// call data
+const productsRouter = express.Router();
+// call router = int_id(django)
+// end import
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")))
-// set static path
+app.use(express.static(path.join(__dirname, "/public2/")))
+// set static path for static web
 
-app.get("/", (req,res) => {
-// set url path
 
-    res.send('<h1>Say Hi</h1> <br> <h2>Hello</h2>');
-    // send respond
+app.set("views", "./src/views");
+// set views path
+app.set("view engine", "ejs");
+// set view engine
+
+
+
+// Set urls pattern    
+
+app.get("/", (req, res) => {
+    // set url path
+
+    res.render('index', { username: 'Chonpat', custommer: ["a", "b", "c", "d"] });
+    // {} type = data = context(django)
+    // send respond when static fail
 })
 
-app.listen(port,()=>{
+productsRouter.route("/").get((req, res) => {
+    res.render("products",products,);
+        // render("products.ejs",data/product.json)
+});
+
+app.use("/products", productsRouter);
+    // Set url route /product
+
+
+// End urls pattern
+
+
+
+
+
+
+app.listen(port, () => {
 
     debug("On port" + chalk.red(port))
     // node app.js <= run app
